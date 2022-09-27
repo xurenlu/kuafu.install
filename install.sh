33ed186f2dd40ea38ab6fdff102fd525cc41030f
黄滚-404.ms <git@404.ms>
2022-09-27 13:44:19 +0800

    --

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo wget -O /usr/bin/kuafu https://huasheng-resume.oss-cn-beijing.aliyuncs.com/kuafu-linux-amd64
+mkdir /home/admin/kuafu/
+mkdir /var/log/kuafu/
+cp ./conf/kuafu.conf /etc/supervisor/conf.d/
